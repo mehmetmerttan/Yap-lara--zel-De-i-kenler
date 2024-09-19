79bf87f48ff43c669668733d9ab7823184c9b6b8
Mert <0mehmetmerttan0@gmail.com>
2024-09-20 00:52:08 +0300

    Add files via upload

---
+++ Odev_1.playground/Contents.swift
@@ -0,0 +1,142 @@
+import UIKit
+
+var ilce  = "Beşiktaş"
+print("İlçe: \(ilce)")
+
+var kita  = "Afrika"
+print("Kıta: \(kita)")
+
+var faks  = "0222 222 22 22"
+print("Faks: \(faks)")
+
+var postaKodu  = "34568"
+print("Posta Kodu: \(postaKodu)")
+
+var instaAdresi  = "mehmetmerttan"
+print("İnsta Adresi: \(instaAdresi)")
+
+var calistiğinBolum  = "IT"
+print("Çalıştığın Bolüm: \(calistiğinBolum)")
+
+var urunMiktari  = "19"
+print("Ürün Miktarı: \(urunMiktari)")
+
+var musteriSoyadi  = "TAN"
+print("Müşteri Soyadı: \(musteriSoyadi)")
+
+var odemeMiktari  = "12000 ₺"
+print("Ödeme Miktarı: \(odemeMiktari)")
+
+var dogumTarihi  = "02/04/2002"
+print("Doğum Tarihi: \(dogumTarihi)")
+
+var borc  = "2350 ₺"
+print("Borç: \(borc)")
+
+var medeniHal  = "Bekar"
+print("Medei Hal: \(medeniHal)")
+
+var videoYorumu  = "Tebrikler <3"
+print("Video Yorumu: \(videoYorumu)")
+
+var odemeSaati  = "19:18"
+print("Ödeme Saati: \(odemeSaati)")
+
+var eftMiktari  = "1234.00 ₺"
+print("EFT Miktarı: \(eftMiktari)")
+
+var satilanMiktar  = "350"
+print("Satian Miktar: \(satilanMiktar)")
+
+var telefonModeli  = "2024"
+print("Telefon Modeli: \(telefonModeli)")
+
+var dergiAdi  = "KARGA"
+print("Dergi Adı: \(dergiAdi)")
+
+var basimTarihi  = "03/05/2022"
+print("Basım Tarihi: \(basimTarihi)")
+
+var zamMiktari  = "%23"
+print("Zam Miktari: \(zamMiktari)")
+
+var daireSayisi  = "24"
+print("Daire Sayısı: \(daireSayisi)")
+
+var enlem  = "40"
+print("Enlem: \(enlem)")
+
+var boylam  = "20"
+print("Boylam: \(boylam)")
+
+var yemekAdi  = "Mantı"
+print("Yemek Adı: \(yemekAdi)")
+
+var urunFiyati  = "155₺"
+print("Urün Fiyat: \(urunFiyati)")
+
+var sirket  = "TAN Grup"
+print("Şirket: \(sirket)")
+
+var videoAdı  = "junior'dan senir'a"
+print("Video Adı: \(videoAdı)")
+
+var muzikSuresi  = "1:56"
+print("Müzik Suresi: \(muzikSuresi)")
+
+var mekanPuani  = "4,56"
+print("Mekan Puani: \(mekanPuani)")
+
+var dosyaAdi  = "Swift_Ödevleri"
+print("Dosya Adı: \(dosyaAdi)")
+
+var resimFormati  = "JPEG"
+print("Resim Formatı: \(resimFormati)")
+
+var renk  = "Mavi"
+print("Renk: \(renk)")
+
+var renkKodu  = "ffffff"
+print("Renk Kodu: \(renkKodu)")
+
+var bilgisayarModeli  = "MacBook Pro"
+print("Bilgisayar Modeli: \(bilgisayarModeli)")
+
+var ekranBoyutu  = "13 inç"
+print("Ekran Boyutu: \(ekranBoyutu)")
+
+var kullanımSuresi  = "12 ay"
+print("Kullancı Suresi: \(kullanımSuresi)")
+
+var basinc  = "1 atm"
+print("Basınç: \(basinc)")
+
+var etkinlikGunu  = "19 Mayıs Perşembe"
+print("Etkinlik Günü: \(etkinlikGunu)")
+
+var odemeGunu  = "24 Kasım 2024"
+print("Ödemem Günü: \(odemeGunu)")
+
+var yolculukCikisTarihi  = "30 Haziran"
+print("Yolculuk Çıkış Tarihi: \(yolculukCikisTarihi)")
+
+var mahalleAdi  = "Burhaniye"
+print("Mahalle Adı: \(mahalleAdi)")
+
+var otobusHatti  = "Üsküdar-Burhaniye"
+print("Otobüs Hattı: \(otobusHatti)")
+
+var kullanilanDakika  = "98dk"
+print("Kullanılan Dakika: \(kullanilanDakika)")
+
+var kargoTakipNo  = "KT66-205867-09"
+print("Kargo Takip No: \(kargoTakipNo)")
+
+var kuponSuresi  = "9 Nisan 2030"
+print("Kupon Süresi: \(kuponSuresi)")
+
+var kuponKodu  = "KK98765"
+print("Kupon Kodu: \(kuponKodu)")
+
+var faturaTarihi  = "07/03/2025"
+print("Fatura Tarihi: \(faturaTarihi)")
